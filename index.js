a755c2a402ec465431e40d35647ccd1b7013f896
Christopher Diaz <pocketofconflict@gmail.com>
2021-04-15 18:00:19 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,34 @@
+function add() {
+  return a + b
+}
+function subtract() {
+  return a - b
+}
+function multiply() {
+  return a * b
+}
+function divide() {
+  return a / b
+}
+function add (a, b) {
+  return a + b
+}
+function subtract (a, b) {
+  return a - b
+}
+function multiply (a, b) {
+  return a * b
+}
+function divide (a, b) {
+  return a / b
+}
+function increment (n) {
+  return (n + 1)
+}
+function decrement (n) {
+  return (n - 1)
+}
+function makeInt (n) {
+  parse ('n', 10)
+  return ('n')
+}
